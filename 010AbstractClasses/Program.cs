2beac7e556c69fb586b31ac6f892d1f51052c7d5
Fjolnirr <yusufcan2617@gmail.com>
2022-11-04 17:06:40 +0300

    AbstractClasses added.

---
+++ 010AbstractClasses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Oracle();
+            database.Add();
+            database.Delete();
+
+            Database database1 = new SqlServer();
+            database1.Add();
+            database1.Delete();
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added by default");
+
+        }
+        public abstract void Delete();
+
+    }
+
+    class SqlServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by SQL");
+        }
+    }
+
+    class Oracle : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Oracle");
+        }
+    }
+}
