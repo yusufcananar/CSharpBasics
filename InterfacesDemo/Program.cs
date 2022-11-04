400d394d8c7443f31c5badc419e6f2d32329545f
Fjolnirr <yusufcan2617@gmail.com>
2022-11-04 14:41:04 +0300

    Interfaces Demo added.

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3] {
+                new Worker(),
+                new Manager(),
+                new Robot()
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager()
+            };
+
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+        }
+
+        interface IWorker
+        {
+            void Work();
+        }
+        //SOLID, Interface Segregation
+        interface IEat
+        {
+            void Eat();
+        }
+        interface ISalary
+        {
+            void GetSalary();
+        }
+
+        class Worker : IWorker, IEat, ISalary
+        {
+            public void Work()
+            {
+
+            }
+            public void Eat()
+            {
+
+            }
+            public void GetSalary()
+            {
+
+            }
+        }
+
+        class Manager : IWorker, IEat, ISalary
+        {
+            public void Work()
+            {
+
+            }
+            public void Eat()
+            {
+
+            }
+            public void GetSalary()
+            {
+
+            }
+        }
+
+        class Robot : IWorker
+        {
+            public void Work() 
+            { 
+
+            }
+        }
+    }
+}
