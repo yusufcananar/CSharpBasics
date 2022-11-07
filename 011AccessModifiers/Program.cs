9fabbaa92239a779114bc2b0678b1559862d8e25
Fjolnirr <yusufcan2617@gmail.com>
2022-11-07 09:42:48 +0300

    AccessModifiers and AccessModifiersDemo added.

---
+++ 011AccessModifiers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Customer
+    {
+        protected int Id { get; set; }
+        private string Name { get; set; }
+        public void Save()
+        {
+
+        }
+    }
+
+    class Student:Customer
+    {
+        public void Save2()
+        {            
+            Customer customer = new Customer();
+            customer.Save();
+        }
+    }
+
+    public class Course
+    {
+        public string Name { get; set; }
+    }
+}
